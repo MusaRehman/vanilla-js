499cbe6c235ddccfec47c5094e765db17abfa441
MusaRehman1 <workforrehman@gmail.com>
2023-12-24 19:32:23 +0500

    class 3

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+console.log('jhj');
+let age = 80;
+let name ="Ali";
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null); //Object
+console.log('here',typeof undefined); //undefined
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+// let score = "32ABC";
+let score = false;
+// let score = null;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+// Our Guarentee that score is number
+let numValue = Number(score);
+console.log(typeof numValue);
+console.log(numValue);

5cb54f0ec85a028c4ac2f802485c981cd9335dd7
MusaRehman1 <workforrehman@gmail.com>
2023-12-24 20:11:54 +0500

    class 3

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,37 @@
-// let score = "32ABC";
-let score = false;
-// let score = null;
+// // let score = "32ABC";
+// let score = false;
+// // let score = null;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof score);
 
-// Our Guarentee that score is number
-let numValue = Number(score);
-console.log(typeof numValue);
-console.log(numValue);
+// // Our Guarentee that score is number
+// let numValue = Number(score);
+// console.log(typeof numValue);
+// console.log(numValue);
+
+// let isloggedin = 1;
+// let varr = Boolean(isloggedin);
+// console.log(varr);
+
+// let num = 32;
+// let str1 = String(num);
+// console.log(str1);
+// console.log(typeof(str1))
+
+// Operations
+
+let value =3;
+let negValue = -value
+// console.log(negValue);
+// console.log(2**3);
+// console.log(2%8); //2
+
+let str1="hello";
+let str2 =" yama";
+let str3 = str1 + str2;
+console.log(str3);
+
+//We Face  a Prolbem in
+console.log("1"+99);
+console.log("1"+2+2);

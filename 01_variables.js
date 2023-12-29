8231edcb1e6b87a4d15dc6417b3675754f4344c8
MusaRehman1 <workforrehman@gmail.com>
2023-12-30 02:11:26 +0500

    Simple Ended

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accId =12345;
+let accEmail ="Yamaook@gmail.com";
+var accPassword ="46854";
+accholder ="Jap";
+
+// console.log(accId);
+// console.log(accholder);
+
+//gives error-also in const
+// console.log(x);
+// let x= 69;
+//Hoiting -undefined
+console.log(y);
+var y= 69;
---
+++ 03_conversionOperation.js
@@ -0,0 +1,59 @@
+// // let score = "32ABC";
+// let score = false;
+// // let score = null;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// // Our Guarentee that score is number
+// let numValue = Number(score);
+// console.log(typeof numValue);
+// console.log(numValue);
+
+// let isloggedin = 1;
+// let varr = Boolean(isloggedin);
+// console.log(varr);
+
+// let num = 32;
+// let str1 = String(num);
+// console.log(str1);
+// console.log(typeof(str1))
+
+// Operations
+
+// let value =3;
+// let negValue = -value
+// // console.log(negValue);
+// // console.log(2**3);
+// // console.log(2%8); //2
+
+// let str1="hello";
+// let str2 =" yama";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// //We Face  a Prolbem in
+// console.log("1"+99);
+// console.log("1"+2+2); //122
+// console.log("1"+2+"2");//122
+// console.log(1+2+"2"); //32
+
+// console.log(+true)
+// console.log("02">1);
+// console.log("02"<1);
+
+//unpredictable behaviour
+// console.log(null>0); //false
+// console.log(null==0); //false
+// console.log(null>=0); //true
+
+// Reason:
+// ==, Equity and comparison < > <= ... works diff
+// because comparison convert null in a number, 0
+
+// console.log(undefined>=0);
+
+// only == performs  coercion
+
+console.log("2" == 2);
+console.log("2" === 2);

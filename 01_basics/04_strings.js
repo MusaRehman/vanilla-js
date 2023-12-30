f987cc2bf662d7060dd0e8bee376b760429977f1
MusaRehman1 <workforrehman@gmail.com>
2023-12-30 23:24:08 +0500

    Added Strings

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -18,3 +18,31 @@
 
 const game = new String("Prettier");
 
+// Accessing a specific String element 
+// console.log(game[0]);
+// console.log(game.__proto__.bold());
+
+console.log(game.length); //Property
+console.log(game.constructor); //Property
+console.log(game.toUpperCase()); //Function f
+
+console.log('----------------------');
+console.log(game.charAt(3));
+console.log(game.indexOf("i"));
+
+//last index is not included
+const gameTwo = game.substring(1,5);
+console.log(gameTwo);
+
+//give negative values to strat in from reverse
+const gameThree = game.slice(-8,3);
+console.log(gameThree);
+
+const newStr ="   Jamal   ";
+console.log(newStr);
+console.log(newStr.trim());
+
+const url = "http://go%20ogle.com";
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('gle'));
